2a7dfe35fe00a669b8c1c1a699af9e8de9182f50
telekikrisz <kallai.krisztian@telekimezotur.hu>
2026-09-09 12:25:29 +0200

    adatbekérés

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Kérem a tanuló nevét:");
+string TanuloNeve = Console.ReadLine();
+Console.WriteLine("Kérem a tanuló pontszámát:");
+int TanuloPontszama = int.Parse(Console.ReadLine());
+int maxPontszam = 97;
+
