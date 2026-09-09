01f1b9d38f85ec67c66ec0eb34e89223a2cc735d
telekikrisz <kallai.krisztian@telekimezotur.hu>
2026-09-09 13:07:17 +0200

    vege

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿Console.WriteLine("Kérem a tanuló nevét:");
+
 string TanuloNeve = Console.ReadLine();
 Console.WriteLine("Kérem a tanuló pontszámát:");
 int TanuloPontszama = int.Parse(Console.ReadLine());
 int maxPontszam = 97;
 double szazalekosErtek = (double)TanuloPontszama / maxPontszam * 100;
-szazalekosErtek=Math.Round(szazalekosErtek, 0);
+//szazalekosErtek=Math.Round(szazalekosErtek, 0);
 Console.WriteLine($"Tanuló neve: {TanuloNeve}");
-Console.WriteLine($"Eredmény: {TanuloPontszama}/{maxPontszam} ({szazalekosErtek}%)");
+Console.WriteLine($"Eredmény: {TanuloPontszama}/{maxPontszam} ({szazalekosErtek:F0}%)");
+if (szazalekosErtek >= 50) Console.WriteLine("Státusz: Sikeres dolgozat!");
+else Console.WriteLine("Státusz: Sikertelen dolgozat!");
